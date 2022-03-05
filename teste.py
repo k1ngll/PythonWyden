33fa64fe26ac7a667c12e61f0b93123a1be48987
A.C <66250092+k1ngll@users.noreply.github.com>
2022-03-05 14:52:49 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,15 @@
+av1 = float (input("digite sua nota da av1 : "))
+av2 = float (input("digite sua nota da av2 : "))
+
+media =float( (av1 + av2)/2)
+
+def x():
+    print('foi')
+
+if media>=6:
+    print("voce se passou")
+   
+else :
+
+    print('voce reprovou')
+
