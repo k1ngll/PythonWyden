a132bda57f3c1baf1ad07225b2464f33f2d67ba1
A.C <66250092+k1ngll@users.noreply.github.com>
2022-03-05 16:23:19 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,23 @@
+x = 10
+a =[0,0,0]
+b =(1,2,3)
+c = True
+cidade = 'Salvador'
+n = 18
+pi=3.143213123
+type (cidade) 
+type (n)
+type (pi)
+
+print('value = ',x)
+print(type(cidade))
+print(type(n))
+print(type(pi))
+print(type(a))
+print(type(b))
+print(type(c))
+''' cont = cont + 1 > cont=+1 
+x = input('valor : ')
+x = int(input('valor : ' ))
+
+print(' value : {}'.format(x) )'''
